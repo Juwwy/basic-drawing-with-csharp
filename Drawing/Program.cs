6388724b72d2ca62ea3249442f88c171f6a91ce6
Olowu Juwon <juwonolowu52@gmail.com>
2021-03-22 22:49:04 +0100

    Basic Drawing using C#

---
+++ Drawing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Drawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("c_     ___");
+            Console.WriteLine("  \\\\__|__|__");
+            Console.WriteLine("   \\(  o_o)");
+            Console.WriteLine("      > ~  >");
+            Console.WriteLine("     /    / \\\\");
+            Console.WriteLine("    /    /   \\\\");
+            Console.WriteLine("    \\   )    c/");
+            Console.WriteLine("    /   /");
+            Console.WriteLine("   /  /|");
+            Console.WriteLine("  (  ( \\");
+            Console.WriteLine("  |  |  \\");
+            Console.WriteLine("  |  /\\  )");
+            Console.WriteLine("  | |  ) |");
+            Console.WriteLine(" /  ) (_/");
+            Console.WriteLine("(_ /");
+        }
+    }
+}
